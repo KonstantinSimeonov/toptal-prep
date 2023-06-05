909949a99c2d4d06daf47689e3e70db78d6b5299
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-06-05 08:39:34 +0300

    cute little problem

---
+++ math/reverse-integer.rs
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/reverse-integer/
+use std::convert::TryFrom;
+
+fn reverse(x: i32) -> i32 {
+    let mut n: i64 = x.into();
+    let mut res = 0;
+    while n != 0 {
+        res = res * 10 + n % 10;
+        n /= 10;
+    }
+
+    i32::try_from(res).ok().unwrap_or(0)
+}
+
+fn main() {
+    for i in -30..30 {
+        println!("{} {}", i, reverse(i));
+    }
+}
